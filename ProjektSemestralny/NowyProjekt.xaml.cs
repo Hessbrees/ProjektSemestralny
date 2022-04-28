f6fad459befe32f2608088599a52127655575e44
Marceli Lenart <lenart483@gmail.com>
2022-04-28 15:14:25 +0200

    Nowe okno

--- ProjektSemestralny/NowyProjekt.xaml.cs
+++ ProjektSemestralny/NowyProjekt.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ProjektSemestralny
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NowyProjekt.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NowyProjekt : Window
     {
-        public MainWindow()
+        public NowyProjekt()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            NowyProjekt win2 = new NowyProjekt();
-            win2.ShowDialog();
         }
     }
 }
