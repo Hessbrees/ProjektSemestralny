53f19eb25f50fc69991f667e102c6a0613ed40a1
Marceli Lenart <lenart483@gmail.com>
2022-05-02 11:36:09 +0200

    Animacja nowe okno

--- ProjektSemestralny/MainWindow.xaml.cs
+++ ProjektSemestralny/MainWindow.xaml.cs
@@ -43,5 +43,11 @@ namespace ProjektSemestralny
             WybierzProjekt win = new WybierzProjekt();
             win.ShowDialog();
         }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            // przycisk animacji
+
+        }
     }
 }
