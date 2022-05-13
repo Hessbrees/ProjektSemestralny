d88e85dbfbe71f32e2af9b2115969dd2b76eb3a5
Marceli Lenart <lenart483@gmail.com>
2022-05-13 16:24:28 +0200

    Usuniecie zbednych plikow

--- ProjectServerDB/dbo/Tables/AnimationBoard640.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[AnimationBoard640] (
-    [id_animation] INT IDENTITY (0, 1) NOT NULL,
-    [id_project]   INT NOT NULL,
-    [boardSize]    INT NOT NULL,
-    PRIMARY KEY CLUSTERED ([id_animation] ASC),
-    CONSTRAINT [Key_AnimationBoard640_id_project] FOREIGN KEY ([id_project]) REFERENCES [dbo].[NewProject] ([id_project])
-);
-
--- ProjectServerDB/dbo/Tables/GlobalValues.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[GlobalValues] (
-    [id_values]     INT NOT NULL,
-    [actualProject] INT NOT NULL,
-    PRIMARY KEY CLUSTERED ([id_values] ASC)
-);
-
